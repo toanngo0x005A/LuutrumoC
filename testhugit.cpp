a988e3ae4806b44babb9a1bc87c2b2f5d4148a5f
Toanngo0x005A <144523171+toanngo0x005A@users.noreply.github.com>
2023-09-09 21:27:58 +0700

    Add files via upload

---
+++ testhugit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "xin chao" << std::endl;
+    std::cout << "Hom nay troi dep qua" << std::endl;
+    std::cout << "Tam biet hen gap lai" << std::endl;
+
+    std::cout << "The end";
+    std::cout << "enmfjff";
+    return 0;
+    }

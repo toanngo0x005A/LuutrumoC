d11a313b84d3bc2bb01102d1ae0859108510269a
Ngo Huu Toan <ngohuulocem@gmail.com>
2023-09-09 19:57:56 +0700

    thulandauthoi

--- testhugit.cpp
+++ testhugit.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "xin chao" << std::endl;
     std::cout << "Hom nay troi dep qua" << std::endl;
     std::cout << "Tam biet hen gap lai" << std::endl;
+
+    std::cout << "The end";
     return 0;
     }

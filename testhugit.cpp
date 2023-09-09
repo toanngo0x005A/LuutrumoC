23b7b708eb04896336741c0ec4eed076d1be276c
Ngo Huu Toan <ngohuulocem@gmail.com>
2023-09-09 18:40:59 +0700

    git commit -m "ngo huu toan"

---
+++ testhugit.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "xin chao" << std::endl;
+    std::cout << "Hom nay troi dep qua" << std::endl;
+    std::cout << "Tam biet" << std::endl;
+    return 0;
+    }
